cb7ed0fe8edcf582abe79a97d58fbce6d88ea16d
안창완 <ssdii66@gmail.com>
2024-07-04 10:51:00 +0900

    tailwind finish

---
+++ app/about/page.tsx
@@ -0,0 +1,19 @@
+export default function About() {
+  return (
+    <section className="w-full h-screen bg-red-400">
+      <a
+        href="#"
+        className="group block max-w-xs mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500"
+      >
+        <div className=" flex items-center space-x-3">
+          <h3 className="text-slate-900 group-hover:text-white text-sm font-semibold">
+            New project
+          </h3>
+        </div>
+        <p className="text-slate-500 group-hover:text-white text-sm">
+          Create a new project from a variety of starting templates.
+        </p>
+      </a>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
-        <input
-          className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="email"
-          required
-          placeholder="Email address"
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block ">
-          Email is required.
-        </span>
-        <button className="text-white py-2 rounded-full active:scale-90  transition-transform peer-invalid:bg-green-400 font-medium outline-none md:px-10 bg-black ">
-          Log in
-        </button>
+    <main className="bg-gray-100 sm:bg-red-500 h-screen flex items-center justify-center p-5">
+      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm  flex flex-col md:flex-row gap-3">
+        {["Nico", "me", "you", "hello"].map((name, index) => (
+          <div className=" flex items-center gap-3 p-2 group: " key={index}>
+            <div className="size-7 bg-blue-500 rounded-full " />
+            <span className="group-hover:text-red-500 group-focus-within:text-orange-500">
+              {name}
+            </span>
+            <div className="size-5 bg-red-500 rounded-full text-white flex justify-center items-center relative">
+              <div className="size-5 bg-red-500 rounded-full absolute  animate-ping"></div>
+              <span className="z-10">{index}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
