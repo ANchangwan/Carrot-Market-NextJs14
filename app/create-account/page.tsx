36ba8c37dde989de7cb5c13626d53a0e75b69e74
안창완 <ssdii66@gmail.com>
2024-07-04 20:00:39 +0900

    form components 🥕

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,3 +1,5 @@
+import FormBtn from "@/components/form-btn";
+import FormInput from "@/components/form-input";
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/20/solid";
 import { ChatBubbleOvalLeftIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
@@ -10,18 +12,31 @@ export default function CreateAccount() {
         <h2 className="text-xl">Fill in the form below to Join</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <div className="flex flex-col gap-2">
-          <input
-            className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2
-            ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400 px-2
-            "
-            type="text"
-            placeholder="username"
-            required
-          />
-          <span className="text-red-500 font-medium">Input Error</span>
-        </div>
-        <button className="primary-btn h-10">Create Account</button>
+        <FormInput
+          type="text"
+          placeholder="write username"
+          required
+          errors={[]}
+        />
+        <FormInput
+          type="email"
+          placeholder="write Email"
+          required
+          errors={[]}
+        />
+        <FormInput
+          type="password"
+          placeholder="password"
+          required
+          errors={[]}
+        />
+        <FormInput
+          type="password"
+          placeholder="Confirm Password"
+          required
+          errors={[]}
+        />
+        <FormBtn loading={false} text="Create Account" />
       </form>
       <div className="w-full h-px bg-neutral-500" />
       <div>
---
+++ components/form-btn.tsx
@@ -0,0 +1,19 @@
+interface FormButtonProps {
+  loading: boolean;
+  text: string;
+}
+
+export default function FormBtn({ loading, text }: FormButtonProps) {
+  return (
+    <button
+      disabled={loading}
+      className="primary-btn h-10 
+      disabled:bg-neutral-400 
+      disabled:text-white
+      disabled:cursor-not-allowed
+      "
+    >
+      {loading ? "Loading..." : text}
+    </button>
+  );
+}
