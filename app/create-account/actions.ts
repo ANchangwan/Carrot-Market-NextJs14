55f8952e5f083b134939faef25f516b529b0e0cd
안창완 <ssdii66@gmail.com>
2024-07-09 00:05:40 +0900

    refinement

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,38 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirmPassword: z.string().min(10),
-});
+const checkPassword = ({
+  password,
+  confirmPassword,
+}: {
+  password: string;
+  confirmPassword: string;
+}) => password === confirmPassword;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "문자로 입력해주세요",
+        required_error: "이름을 입력하세요",
+      })
+      .min(3, "너무 짧아요!!")
+      .max(10, "너무 길어요!!")
+      .refine((username) => false, "this is custom"),
+    email: z
+      .string({
+        invalid_type_error: "이메일을 입력해주세요",
+      })
+      .email({
+        message: "유효한 이메일을 입력해주세요!!!",
+      }),
+    password: z.string().min(10, "10자 이상을 입력해주세요"),
+    confirmPassword: z.string().min(10, "10자 이상을 입력해주세요"),
+  })
+  .refine(checkPassword, {
+    message: "패스워드가 서로 다릅니다!!",
+    path: ["confirmPassword"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
