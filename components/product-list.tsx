ce5110446498df82390dacf1b9fe20d9fe4a5148
안창완 <ssdii66@gmail.com>
2024-07-30 20:55:32 +0900

    load more button 추가

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -10,11 +10,19 @@ interface ProductListProps {
 
 export default function ProductList({ initialProducts }: ProductListProps) {
   const [products, setProducts] = useState(initialProducts);
+  const [page, setPage] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [isLastPage, setIsLastPage] = useState(false);
   const onLoadMoreClick = async () => {
     setIsLoading(true);
-    const newProducts = await getMoreProducts(1);
-    setProducts((prev) => [...prev, ...newProducts]);
+    const newProducts = await getMoreProducts(page + 1);
+    if (newProducts.length !== 0) {
+      setPage((prev) => prev + 1);
+      setProducts((prev) => [...prev, ...newProducts]);
+    } else {
+      setIsLastPage(true);
+    }
+
     setIsLoading(false);
   };
   return (
@@ -22,15 +30,17 @@ export default function ProductList({ initialProducts }: ProductListProps) {
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      <button
-        onClick={onLoadMoreClick}
-        disabled={isLoading}
-        className="text-sm font-semibold bg-orange-500 rounded-md py-2 px-3 w-fit mx-auto
+      {isLastPage ? null : (
+        <button
+          onClick={onLoadMoreClick}
+          disabled={isLoading}
+          className="text-sm font-semibold bg-orange-500 rounded-md py-2 px-3 w-fit mx-auto
       hover:opacity-90 active:scale-95
       "
-      >
-        {isLoading ? "로딩 중..." : "더 보기"}
-      </button>
+        >
+          {isLoading ? "로딩 중..." : "더 보기"}
+        </button>
+      )}
     </div>
   );
 }
