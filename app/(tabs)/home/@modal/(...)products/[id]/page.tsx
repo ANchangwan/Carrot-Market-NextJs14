5c04d0a39685f0f3f9e462e13338743337d581dd
안창완 <ssdii66@gmail.com>
2024-08-03 16:41:33 +0900

    button modal

--- app/(tabs)/home/@modal/(...)products/[id]/page.tsx
+++ app/(tabs)/home/@modal/(...)products/[id]/page.tsx
@@ -1,21 +1,12 @@
-"use client";
-import { PhotoIcon, XMarkIcon } from "@heroicons/react/24/solid";
-import { useRouter } from "next/navigation";
+import ModalButton from "@/components/modal-button";
+import { PhotoIcon } from "@heroicons/react/24/solid";
 
-export default function Modal({ params }: { params: { id: string } }) {
-  const router = useRouter();
-  const onCloseClick = () => {
-    router.back();
-  };
+export default async function Modal({ params }: { params: { id: string } }) {
+  
   return (
     <div className="absolute bg-opacity-60 w-full h-full z-50 flex justify-center items-center bg-black left-0 top-0">
       <div className="max-w-screen-sm w-full h-1/2 flex justify-center">
-        <button
-          onClick={onCloseClick}
-          className="absolute right-5 top-5 text-neutral-200"
-        >
-          <XMarkIcon className="size-10" />
-        </button>
+        <ModalButton />
         <div
           className="aspect-square  bg-neutral-700 text-neutral-200 
         rounded-md flex justify-center items-center"
